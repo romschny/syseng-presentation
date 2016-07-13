2f04738fb0dc99fce6c55dd5b18eb937651e9d72
RomanKol <kollatschny.roman@web.de>
2016-07-13 15:25:30 +0200

    updated slides

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('app'));
+app.use('/', express.static('presentation'));
 
 app.listen(3000, function () {
   console.log('localhost:3000/');
